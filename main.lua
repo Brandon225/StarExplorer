14a08f4c065abd083f28017ba65b17e3af39d2f8
Brandon Tindle <bjtactor@gmail.com>
2018-03-17 18:43:02 -0400

    Game configuration setup

--- main.lua
+++ main.lua
@@ -5,3 +5,11 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- Setup Game Physics
+local physics = require("physics")
+physics.start()
+physics.setGravity( 0, 0 ) -- no gravity is space so 0,0 sets gravity to none
+
+-- Seed the random number generator
+math.randomseed( os.time() )
